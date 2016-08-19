7e6a968ba004c4a3995450953622f5d0cef1d2bc
Indian Institute of Crime Science <crimescienceinstitute@users.noreply.github.com>
2016-08-19 01:18:30 -0700

    Initial commit
    ITDB®: International Terrorisms Database.
    Home URL: https://github.com/ITDB
    Author: Prabhat Kumar, http://prabhatkumar.org/.
    Copyright: Sequømics Corporation, http://sequomics.com/.

---
+++ index.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+
+/*!
+ * ITDB®      : International Terrorisms Database.
+ * @version   : [1.0.0]
+ * __________________________________________________________________________________
+ * I I C S®
+ * ~~~~~~~
+ * Indian Institute of Crime Science.
+ * भारतीय अपराध विज्ञान संस्थान।
+ * __________________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * __________________________________________________________________________________
+ * @date      : 18-August-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * __________________________________________________________________________________
+ *      Licensed under the Apache License, Version 2.0, (the "License");
+ *      you may not use this file except in compliance with the License.
+ *                  You may obtain a copy of the License at:
+ *                 http://www.apache.org/licenses/LICENSE-2.0
+ *                                     or
+ *            https://github.com/ITDB/ITDB/blob/master/LICENSE.md
+ *    Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS-IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *                        limitations under the License.
+ * __________________________________________________________________________________
+ *
+ *                  --/The Heart of Database System/-- of "ITDB®".
+ * __________________________________________________________________________________
+ */
