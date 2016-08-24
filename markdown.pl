4e166866cf28bc4154172ac865d8d7187b6ba3bb
Indian Institute of Crime Science <crimescienceinstitute@users.noreply.github.com>
2016-08-24 22:50:14 +0530

    Initial commit
    http://daringfireball.net/projects/markdown/index.text

--- markdown.pl
+++ markdown.pl
@@ -9,3 +9,16 @@
 # Author    : Prabhat Kumar [http://prabhatkumar.org/].
 # Copyright : Sequømics Corporation [http://sequomics.com/].
 # ----------------------------------------------------------------
+
+# ----------------------------------------------------------------
+# INTRODUCTION
+# ----------------------------------------------------------------
+# Markdown is a text-to-HTML conversion tool for web writers. Markdown allows you to write using an easy-to-read, easy-to-write plain text format, then convert it to structurally valid XHTML (or HTML).
+# Thus, “Markdown” is two things:
+#         (1) a plain text formatting syntax; and
+#         (2) a software tool, written in Perl, that converts the plain text formatting to HTML.
+# See the Syntax page for details pertaining to Markdown’s formatting syntax.
+# The overriding design goal for Markdown’s formatting syntax is to make it as readable as possible. The idea is that a Markdown-formatted document should be publishable as-is, as plain text, without looking like it’s been marked up with tags or formatting instructions.
+# While Markdown’s syntax has been influenced by several existing text-to-HTML filters, the single biggest source of inspiration for Markdown’s syntax is the format of plain text email.
+# The best way to get a feel for Markdown’s formatting syntax is simply to look at a Markdown-formatted document. For example, you can view the Markdown source for the article text on this page here:
+# http://daringfireball.net/projects/markdown/index.text
