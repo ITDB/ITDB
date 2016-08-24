6ae89ccbea0c643fdb8dc3cc5da7af0640bc53e2
Indian Institute of Crime Science <crimescienceinstitute@users.noreply.github.com>
2016-08-24 06:34:15 -0700

    Initial commit
    Markdown 1.0.1 (18 KB) — 18 Dec 2004.
    Markdown is a text-to-HTML conversion tool for web writers.
    Markdown allows you to write using an easy-to-read, easy-to-write plain text format, then convert it to structurally valid XHTML (or HTML).
    Help: http://daringfireball.net/projects/markdown/.

---
+++ markdown.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+
+
+# I I C S®
+# ~~~~~~~~
+# Indian Institute of Crime Science.
+# भारतीय अपराध विज्ञान संस्थान।
+# ----------------------------------------------------------------
+# Author    : Prabhat Kumar [http://prabhatkumar.org/].
+# Copyright : Sequømics Corporation [http://sequomics.com/].
+# ----------------------------------------------------------------
