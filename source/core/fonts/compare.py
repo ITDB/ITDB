06658ba42c53aa5533522be424dc1e190b4160be
Indian Institute of Crime Science <crimescienceinstitute@users.noreply.github.com>
2016-09-02 23:19:18 +0530

    Initial commit
    compare.py, to compare fonts.

---
+++ source/core/fonts/compare.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__authors__       = [
+    '"Prabhat Kumar" <prabhat.genome@gmail.com>',
+    '"Sequømics Corporation" <admin@sequomics.com>',
+    '"Indian Institute of Crime Science" <crimescienceinstitute@gmail.com>'
+    ]
+__contributors__  = '"Emma Jörenberg" <https://github.com/jorenberg>'
+__license__       = 'Apache License'
