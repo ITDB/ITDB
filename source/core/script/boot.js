08ecccf1ce9054b1430e1efe6244afcc86d2cbcf
Indian Institute of Crime Science <crimescienceinstitute@users.noreply.github.com>
2016-09-04 23:14:21 +0530

    Initial commit
    boot.js, transcompiles from CoffeeScript -> boot.coffee

---
+++ source/core/script/boot.js
@@ -0,0 +1,22 @@
+/*!
+ * IICS®
+ * Indian Institute of Crime Science.
+ * _______________________________________________________________
+ * ITDB®, https://github.com/ITDB/ITDB
+ * The Non-AMD JavaScript - for defining "ITDB®" configurations.
+ * _______________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * _______________________________________________________________
+ * @date      : 04-Sept-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : Grunt
+ * _______________________________________________________________
+ *
+ * --/The Heart of Non-AMD System/-- of "CBBL®".
+ * _______________________________________________________________
+ */
